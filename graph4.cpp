127710970ebbe65b97f652690baacce5afeb9e83
Sunny Dutta <sunnyrizzler123@gmail.com>
2026-02-20 16:44:45 +0530

    Create graph4.cpp

---
+++ graph4.cpp
@@ -0,0 +1,5 @@
+//Cycle Detection.
+//We can detect cycles in directed as well as undirected graphs.
+//Three are 3 ways to detect cycles in undirected graph: dfs, bfs, Disjoint set union.
+//Three are 3 ways to detect cycles in directed graph: dfs, bfs, Kahns Algorithm(Topological Sort).
+//Cycle Detection in Undirected Graph. We will return true if cycle exists and will return false if cycle doesn't exist.
